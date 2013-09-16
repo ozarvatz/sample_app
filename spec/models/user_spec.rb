c1e721dfd091013e472b00b70a2c413b4240081a
Oz Arvatz <oz.a@fiverr.com>
2013-09-16 09:17:06 +0300

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -38,7 +38,7 @@ describe User do
 		it "should be invalid" do
 			addresses = %w[ user@foo,com user_at_foo.org 
 				example.user@foo. foo@bar_baz.com 
-				foo@bar_baz.com]
+				foo@bar_baz.com foo@bar..com]
 			addresses.each do |invalid_address|
 				@user.email = invalid_address
 				expect(@user).not_to be_valid
